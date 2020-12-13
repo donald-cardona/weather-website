f424b787be7d40d602a732c75c822bb5b27b7a42
Donald Cardona <d.cardona@ibm.com>
2020-12-13 00:49:39 -0500

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=01a1ca7f64f0f7872e15ab9441690072&query='+latitude+','+longitude+'&units=f'
+
+    request({url, json: true}, (error, {body}) => {
+        if(error) {
+            callback('Unable to connect to weather services!', undefined)
+        }
+        else if(body.error) {
+            callback('Unable to find location', undefined)
+        }
+        else {
+            callback(undefined, body.current.weather_descriptions[0] +'. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degrees.')
+        }
+    })
+}
+
+module.exports = forecast
